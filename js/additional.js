6c4937ee97e226050f3afa2eb8d67144a89882ea
“Your <you@example.com>
2022-05-30 18:51:00 +0500

    final

--- js/additional.js
+++ js/additional.js
@@ -152,15 +152,8 @@ $(document).ready(function() {
     $("#more").on("click", "a", function(event) {
         //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
-
-        //забираем идентификатор бока с атрибута href
-        var id = $(this).attr('href'),
-
-            //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;
-
         //анимируем переход на расстояние - top за 1500 мс
-        $('body,html').animate({ scrollTop: top }, 500);
+        $('body,html').animate({ scrollTop: 1000 }, 500);
     });
 });
 
