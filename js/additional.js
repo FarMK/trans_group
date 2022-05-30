4a9593e0b12fe89441531d65c67ad0c9b07c512b
“Your <you@example.com>
2022-05-30 11:50:09 +0500

    final

--- js/additional.js
+++ js/additional.js
@@ -97,6 +97,59 @@ $(document).ready(function() {
 });
 
 
+$(document).ready(function() {
+    $("#services_nav_mobile").on("click", "a", function(event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 500);
+    });
+});
+
+$(document).ready(function() {
+    $("#about_nav_mobile").on("click", "a", function(event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 500);
+    });
+});
+
+
+$(document).ready(function() {
+    $("#contacts_nav_mobile").on("click", "a", function(event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 500);
+    });
+});
+
+
+
+
+
 
 
 
